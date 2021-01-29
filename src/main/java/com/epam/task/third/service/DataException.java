dcbfc8119aa1a886ce5173feb3068ea5f84c9807
Stanly0407 <Shelestova_S_V@mail.ru>
2021-01-29 22:09:24 +0300

    task

--- src/main/java/com/epam/task/third/service/DataException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.task.third.service;
-
-public class DataException extends Exception{
-
-    public DataException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/epam/task/third/service/TextModifiable.java
+++
@@ -1,9 +0,0 @@
-package com.epam.task.third.service;
-
-public interface TextModifiable {
-
-    String changeEverySeventhLetterInWords(String text) throws DataException;
-
-    String deleteWordsOfEightLettersBeginningConsonant(String text) throws DataException;
-
-}
