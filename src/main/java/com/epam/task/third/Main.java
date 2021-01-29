ff3d38c6da195f1d98d061a3ad7dbec52f86122e
Stanly0407 <Shelestova_S_V@mail.ru>
2021-01-29 22:43:13 +0300

    task3

--- src/main/java/com/epam/task/third/Main.java
+++ src/main/java/com/epam/task/third/Main.java
@@ -1,3 +1,27 @@
 package com.epam.task.third;
 
 
+import com.epam.task.third.service.DataException;
+import com.epam.task.third.service.ModificationByStringMethods;
+import org.apache.log4j.Logger;
+
+
+public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Program started");
+
+        String textExample = "";
+        try {
+            ModificationByStringMethods modificationByStringMethods = new ModificationByStringMethods();
+            System.out.println(modificationByStringMethods.changeEverySeventhLetterInWords(textExample));
+        } catch (DataException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+
+        LOGGER.info("Program finished");
+    }
+}
---
+++ src/main/java/com/epam/task/third/service/DataException.java
@@ -0,0 +1,8 @@
+package com.epam.task.third.service;
+
+public class DataException extends Exception{
+
+    public DataException(String message) {
+        super(message);
+    }
+}
